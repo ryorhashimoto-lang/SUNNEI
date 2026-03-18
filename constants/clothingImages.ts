a4f3bf07eb0263214505f5c58a517d7120712e2b
ryorhashimoto-lang <ryo.r.hashimoto@seniorand.com>
2026-03-18 16:32:35 +0900

    Add clothing images mapping and retrieval function
    Added a mapping of clothing options to image URLs and a function to retrieve the image URL based on the selected option.

--- constants/clothingImages.ts
+++ constants/clothingImages.ts
@@ -1,1 +1,20 @@
+/**
+ * 着せ替え画像データを管理するファイル
+ * ClothingOption（着せ替えの種類）と画像のURLをマッピング
+ */
 
+import { ClothingOption } from '../types';
+
+export const CLOTHING_IMAGES: Record<string, string> = {
+  [ClothingOption.MensSuitBlack]: '/clothing/mens_suit_black.png',
+  [ClothingOption.MensKimonoBlack]: '/clothing/mens_kimono_black.png',
+  [ClothingOption.WomensSuitBlack]: '/clothing/womens_suit_black.png',
+  [ClothingOption.WomensKimonoBlack]: '/clothing/womens_kimono_black.png',
+};
+
+/**
+ * 指定された着せ替えオプションの画像URLを取得
+ */
+export const getClothingImage = (option: ClothingOption): string => {
+  return CLOTHING_IMAGES[option] || '';
+};
