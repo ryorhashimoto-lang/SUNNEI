731d93744a9700b9b42edeb1ac87d4bc05cdbf16
ryorhashimoto-lang <ryo.r.hashimoto@seniorand.com>
2026-03-23 14:25:31 +0900

    Remove unused CropConfig import from ActionPanel

--- components/ActionPanel.tsx
+++ components/ActionPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ClothingOption, BackgroundOption, UserPlan, PLAN_LIMITS, CropConfig } from '../types';
+import { ClothingOption, BackgroundOption, UserPlan, PLAN_LIMITS } from '../types';
 
 interface ActionPanelProps {
   onBgAction: (option: BackgroundOption) => void;
@@ -28,7 +28,7 @@ const ActionPanel: React.FC<ActionPanelProps> = ({
   userPlan,
   usageCount,
   deceasedName,
-  backgroundOption
+  onDeceasedNameChange
 }) => {
   const [gender, setGender] = useState<'men' | 'women'>('men');
   const [bgTab, setBgTab] = useState<'solid' | 'landscape'>('solid');
